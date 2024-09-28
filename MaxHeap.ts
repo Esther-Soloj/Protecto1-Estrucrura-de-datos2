654ff247632fd4ed7aca9afe01956fdc0225c2c8
Esther Soloj <78984770+Esther-Soloj@users.noreply.github.com>
2024-09-27 18:08:15 -0600

    se implemento la inserciòn, el cambio de tamaño y la extraccion de MaxHeap

--- MaxHeap.ts
+++ MaxHeap.ts
@@ -20,4 +20,51 @@ class MaxHeap {
     public getQuantity(): number {
         return this.n;
     }
+
+    public insert(value: { precio: number; cantidad: number; empresa: string }): void {
+        if (this.n === this.heap.length - 1) {
+            this.resize(2 * this.heap.length); // Duplicar tamaño si está lleno
+        }
+        this.n++;
+        this.heap[this.n] = value;
+        this.swap(this.n);
+    }
+
+    private swap(i: number): void {
+        let father: number = Math.floor(i / 2);
+        while (i > 1 && this.heap[father].precio < this.heap[i].precio) {
+            [this.heap[i], this.heap[father]] = [this.heap[father], this.heap[i]]; // Intercambio
+            i = father;
+            father = Math.floor(i / 2);
+        }
+    }
+
+    private resize(newSize: number): void {
+        const newHeap: { precio: number; cantidad: number; empresa: string }[] = new Array(newSize);
+        for (let i = 1; i < this.heap.length; i++) {
+            newHeap[i] = this.heap[i];
+        }
+        this.heap = newHeap;
+    }
+
+    public getMax(): { precio: number; cantidad: number; empresa: string } | undefined {
+        const max = this.heap[1];
+        this.heap[1] = this.heap[this.n];
+        this.n--;
+        this.sink(1);
+        return max;
+    }
+
+    private sink(i: number): void {
+        while (2 * i <= this.n) {
+            let j = 2 * i;
+            if (j < this.n && this.heap[j].precio < this.heap[j + 1].precio) {
+                j++;
+            }
+            if (this.heap[i].precio >= this.heap[j].precio) break;
+            [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+            i = j;
+        }
+    }
 }
+
